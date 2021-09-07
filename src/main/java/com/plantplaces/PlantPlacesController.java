55d43046fee6fc86a6d472f61352fe85d5984aa4
Administrator <Administrator@DESKTOP-099UQRG>
2021-09-07 14:37:26 -0400

    update .gitignore

---
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -0,0 +1,15 @@
+package com.plantplaces;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//http://127.0.0.1:8080/start
+@Controller
+public class PlantPlacesController {
+
+	@RequestMapping("/start")
+	public String start() {
+		return "start";
+		
+	}
+}
